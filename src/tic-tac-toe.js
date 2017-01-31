a55a06eea065a92b409bb7c95814b00c6eae204c
Tatarinovich Ekaterina <etatarinovich18@gmail.com>
2017-01-31 02:41:03 +0200

    tic-tac-toe: implemented isDraw, isFinish, added lookingForWinner

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,7 +1,7 @@
 class TicTacToe {
     constructor() {
         this.currentPlayerSymbol = 'x';
-        this.emptyMarks = 0;
+        this.unoccupiedMarks = 0;
         this.playerWinner = null;
         this.board = [];
         var row = 3,
@@ -10,12 +10,12 @@ class TicTacToe {
             this.board[i] = [];
             for (var j = 0; j < col; j++) {
                 this.board[i][j] = null;
-                this.emptyMarks++;
+                this.unoccupiedMarks++;
             }
         }
     }
 
-    getCurrentPlayerSymbol() {//////
+    getCurrentPlayerSymbol() {
         return this.currentPlayerSymbol;
     }
 
@@ -27,7 +27,7 @@ class TicTacToe {
             if (this.board[this.rowIndex][this.columnIndex] == null) {
                 this.board[this.rowIndex][this.columnIndex] = 'x';
                 this.currentPlayerSymbol = 'o';
-                this.emptyMarks--;
+                this.unoccupiedMarks--;
             } else {
                 this.currentPlayerSymbol = 'x';
             }
@@ -35,28 +35,57 @@ class TicTacToe {
             if (this.board[this.rowIndex][this.columnIndex] == null) {
                 this.board[this.rowIndex][this.columnIndex] = 'o';
                 this.currentPlayerSymbol = 'x';
-                this.emptyMarks--;
+                this.unoccupiedMarks--;
             } else {
                 this.currentPlayerSymbol = 'o';
             }
         }
 
-        // this.emptyMarks--;
+        //added lookingForWinner (if he exists) because the nextTurn method without it was enormous 
+        this.lookingForWinner(); 
+
     }
 
     isFinished() {
-        if (this.emptyMarks == 0 || this.playerWinner != null) {
+        if (this.unoccupiedMarks == 0 || this.playerWinner != null) {
             return true;
         } else {
             return false;
         }
     }
     
-    getemptyMarks() {
-        return this.emptyMarks;
+    getunoccupiedMarks() {
+        return this.unoccupiedMarks;
     }
     
     getWinner() {
+        return this.playerWinner;
+    }
+
+    noMoreTurns() {
+        if (this.unoccupiedMarks == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    isDraw() {
+        if (this.unoccupiedMarks != 0 || this.playerWinner != null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    getFieldValue(rowIndex, colIndex) {
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+
+        return this.board[this.rowIndex][this.colIndex];
+    }
+
+    lookingForWinner() {
         if ((this.board[0][0] == "x" && this.board[0][1] == "x" && this.board[0][2] == "x") ||
             (this.board[1][0] == "x" && this.board[1][1] == "x" && this.board[1][2] == "x") ||
             (this.board[2][0] == "x" && this.board[2][1] == "x" && this.board[2][2] == "x") ||
@@ -75,32 +104,11 @@ class TicTacToe {
                     (this.board[0][0] == "o" && this.board[1][1] == "o" && this.board[2][2] == "o") ||
                     (this.board[2][0] == "o" && this.board[1][1] == "o" && this.board[0][2] == "o")) {
                         this.playerWinner = 'o';
-        }
-        return this.playerWinner;
-    }
-
-    noMoreTurns() {
-        if (this.emptyMarks == 0) {
-            return true;
         } else {
-            return false;
+            this.playerWinner = null;
         }
     }
 
-    isDraw() {
-        if (this.emptyMarks != 0 || this.playerWinner != null) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-        this.rowIndex = rowIndex;
-        this.colIndex = colIndex;
-
-        return this.board[this.rowIndex][this.colIndex];
-    }
 }
 
 module.exports = TicTacToe;
